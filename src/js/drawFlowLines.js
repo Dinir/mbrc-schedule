85774a1e94d82d5135460bcc52106ad745472741
Dinir Nertan <DinirNertan@gmail.com>
2018-03-18 19:49:18 +0900

    the image should be a placeholder because it's so stupid

---
+++ src/js/drawFlowLines.js
@@ -0,0 +1,6 @@
+const canvas = document.getElementById('flowLines');
+const ctx = canvas.getContext('2d');
+
+const {width, height} =  b._el.getBoundingClientRect();
+canvas.style.width = width + 'px';
+canvas.style.height = height + 'px';
