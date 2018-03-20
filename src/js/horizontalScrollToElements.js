84d86e98e92578ff447f2158784a13224a780239
Dinir Nertan <DinirNertan@gmail.com>
2018-03-21 01:56:16 +0900

    second day results

--- src/js/horizontalScrollToElements.js
+++ src/js/horizontalScrollToElements.js
@@ -5,21 +5,27 @@
  *
  * @param {HTMLElement} container element to accept scroll events
  * @param {HTMLCollection} elements collection of elements that will work as scroll snap points
+ * @param {number} startPosition if specified the callback will work with this number as the default position
  *
  * @return {callback}
  */
-const horizontalScrollToElements = (container, elements) => {
-  let currentElement = 0;
+const horizontalScrollToElements = (container, elements, startPosition = 0) => {
+  let currentElement = startPosition;
   const maxElementNumber = elements.length - 1;
 
+  container.scrollLeft = elements[currentElement].offsetLeft;
+
   return e => {
+    e.preventDefault();
+    const scrollLeftMax = container.scrollLeftMax ||
+      container.scrollWidth - container.clientWidth;
     if(e.deltaY > 0) {
-      if(container.scrollLeft < container.scrollLeftMax)
+      if(container.scrollLeft < scrollLeftMax)
         currentElement = Math.min(maxElementNumber, currentElement + 1);
     } else {
       do {
         currentElement = Math.max(0, currentElement - 1);
-      } while(elements[currentElement].offsetLeft > container.scrollLeftMax);
+      } while(elements[currentElement].offsetLeft > scrollLeftMax);
     }
     container.scrollLeft = elements[currentElement].offsetLeft;
   };
