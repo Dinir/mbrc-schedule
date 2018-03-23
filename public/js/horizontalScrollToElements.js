38b19f3cef9e9ac6a8efce5b059623afe35b3e99
Dinir Nertan <DinirNertan@gmail.com>
2018-03-23 22:46:11 +0900

    change horizontal scroll behavior to prevent default one

--- public/js/horizontalScrollToElements.js
+++ public/js/horizontalScrollToElements.js
@@ -7,20 +7,27 @@
  *
  * @param {HTMLElement} container element to accept scroll events
  * @param {HTMLCollection} elements collection of elements that will work as scroll snap points
+ * @param {number} startPosition if specified the callback will work with this number as the default position
  *
  * @return {callback}
  */
 var horizontalScrollToElements = function horizontalScrollToElements(container, elements) {
-  var currentElement = 0;
+  var startPosition = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 0;
+
+  var currentElement = startPosition;
   var maxElementNumber = elements.length - 1;
 
+  container.scrollLeft = elements[currentElement].offsetLeft;
+
   return function (e) {
+    e.preventDefault();
+    var scrollLeftMax = container.scrollLeftMax || container.scrollWidth - container.clientWidth;
     if (e.deltaY > 0) {
-      if (container.scrollLeft < container.scrollLeftMax) currentElement = Math.min(maxElementNumber, currentElement + 1);
+      if (container.scrollLeft < scrollLeftMax) currentElement = Math.min(maxElementNumber, currentElement + 1);
     } else {
       do {
         currentElement = Math.max(0, currentElement - 1);
-      } while (elements[currentElement].offsetLeft > container.scrollLeftMax);
+      } while (elements[currentElement].offsetLeft > scrollLeftMax);
     }
     container.scrollLeft = elements[currentElement].offsetLeft;
   };
